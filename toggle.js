2f43d55f5de4b0f38f18c0aae3ab4e6e5af40dbd
Ayan shaikh <99812147+Ayan71@users.noreply.github.com>
2022-12-19 12:38:52 +0530

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,22 @@
+
+let isWhite=true;
+
+function black() {
+    if(isWhite===true){
+    document.body.style.backgroundColor = "black";
+    document.querySelector(".toggle").style.justifyContent = "end"
+    document.querySelector(".heading h1").style.color = "#fff"
+    document.querySelector(".toggle").style.backgroundColor = "#fff"
+    isWhite=false;
+    }
+    else{
+        document.body.style.backgroundColor="white";
+        document.querySelector(".toggle").style.justifyContent="start";
+      document.querySelector(".heading h1").style.color="#000";
+      document.querySelector(".toggle").style.backgroundColor = "#fff"
+     isWhite=true;
+
+    
+
+    }
+}
